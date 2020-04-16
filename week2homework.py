ce2a7ba05c046d9ef7e95421858a2f3501dee134
kodama6699 <63088792+kodama6699@users.noreply.github.com>
2020-04-17 03:42:43 +0900

    Create week2homework.py

---
+++ week2homework.py
@@ -0,0 +1,18 @@
+# chapter2 answer9
+
+if spam == 1:
+  print('Hello')
+elif spam == 2:
+  print('Howdy')
+else:
+  print('Greetings!')
+  
+# chapter2 answer13
+
+for i in range(1, 11)
+  print(i)
+and:
+  i = 1
+  while i <= 10:
+    print(i)
+    i = i + 1
